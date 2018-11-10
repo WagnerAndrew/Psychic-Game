82f23a297006988d84d5226d515d8a209618f620
WagnerAndy <wagner.andrew.james@gmail.com>
2018-11-09 20:20:22 -0700

    Psychic-Game-v00-03

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,48 @@
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var winCount = 0;
 var lossCount = 0;
 var guessCount = 10;
 var lettersGuessed = [];
+
 
 var winCountText = document.getElementById("winCount");
 var lossCountText = document.getElementById("lossCount");
 var guessCountText = document.getElementById("guessCount");
 var lettersGuessedText = document.getElementById("lettersGuessed");
 
-// var computerLetter = "";
 var computerLetter = randomLetter ();
 
-function randomLetter (){
-    var randomLetterGenerator = letters[Math.floor(Math.random() * letters.length)];
-    // console.log ("Inside of randomLetter function it generates : " + randomLetterGenerator);
-    return randomLetterGenerator;
-    // return computerLetter;
-};
 
-// randomLetter ();
 
-// console.log ("Result of computerLetter: " + computerLetter);
-
+    function randomLetter (){
+        var randomLetterGenerator = letters[Math.floor(Math.random() * letters.length)];
+    
+        return randomLetterGenerator;  
+    };
 
 
 document.onkeyup = function (event) {
+
     var userGuessLower = event.key;
     var userGuess = userGuessLower.toLowerCase();
-    
-    console.log("The computer letter is: " + computerLetter);
-    // console.log (userGuess);
-    // && userGuess === letters[indexOf] 
-    
-        if (userGuess === computerLetter) {
+    console.log ("LETTER ARRAY: "+letters);
+    console.log ("USER GUESS :" +userGuess);
+    console.log ("LETTERS GUESSED :" +lettersGuessed);
+    console.log ("ComputerLetter: "+computerLetter);
+
+    // && lettersGuessed.indexOf(userGuess) <= 0
+    if (letters.indexOf(userGuess) < 0){
+        
+        return;
+    } else if (userGuess === computerLetter){
             winCount++;
             winCountText.textContent = winCount ++;
-            lettersGuessed = [""];
+            lettersGuessed = [];
             lettersGuessedText.textContent = lettersGuessed;
             guessCount = 10;
-            guessCountText.textContent = guessCount; 
-            computerLetter = randomLetter ();
-            // console.log ("computer main process:" + computerLetter)
-
+            guessCountText.textContent = guessCount;
+            randomLetter ();
+            computerLetter = randomLetter ()
         } else {
             guessCountText.textContent = guessCount - 1;
             guessCount--;
@@ -51,16 +51,17 @@ document.onkeyup = function (event) {
 
         } 
         
-         if (guessCount === 0) {
-            lossCount++ ;
-            lossCountText.textContent = lossCount;
-            guessCount = 10;
-            guessCountText.textContent = guessCount;
-            lettersGuessed = [""];
-            lettersGuessedText.textContent = lettersGuessed;
-            computerLetter = randomLetter ();
+    if (guessCount === 0) {
+        lossCount++ ;
+        lossCountText.textContent = lossCount;
+        guessCount = 10;
+        guessCountText.textContent = guessCount;
+        lettersGuessed = [];
+        lettersGuessedText.textContent = lettersGuessed;
+        randomLetter ();
+        computerLetter = randomLetter ();
 
-        }
+    }
 
 }
 
